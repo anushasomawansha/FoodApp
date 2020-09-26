8237e25c74410aa101dfa8e9d942e4a2fad95b0a
Induwara98 <it19007670@my.sliit.lk>
2020-09-26 22:42:09 +0530

    Admin Functionality phase 1

--- app/src/main/java/com/example/foodrunner/DeleteItem.java
+++ app/src/main/java/com/example/foodrunner/DeleteItem.java
@@ -1,14 +1,70 @@
 package com.example.foodrunner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DeleteItem extends AppCompatActivity {
+
+    TextView textViewName , textViewPrice;
+    Button btnDel;
+    DatabaseReference dbRef;
+    private RecyclerView recyclerView;
+
+    private FirebaseRecyclerOptions<Products> options;
+    private FirebaseRecyclerAdapter<Products,MyViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_item);
+
+
+
+        btnDel = findViewById(R.id.btnDel);
+
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Products");
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        options=new FirebaseRecyclerOptions.Builder<Products>().setQuery(dbRef,Products.class).build();
+        adapter=new FirebaseRecyclerAdapter<Products, MyViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Products model) {
+                holder.textViewName.setText(String.valueOf(model.getName()));
+                holder.textViewPrice.setText(""+model.getPrice());
+
+
+
+            }
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_view_layout,parent,false);
+
+                return new MyViewHolder(view);
+            }
+        };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
     }
 }
