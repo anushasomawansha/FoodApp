a1ed6dbfd395743050c3d51b0e3e4448161299ea
anushasomawansha <anushamadhushan@gmail.com>
2020-09-28 14:41:20 +0530

    add logo and splash screen to main activity

--- app/src/main/java/com/example/foodrunner/MainActivity.java
+++ app/src/main/java/com/example/foodrunner/MainActivity.java
@@ -6,27 +6,67 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.foodrunner.ui.ListShoppingList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
+    private static int SPLASH_TIME_OUT = 5000;
+    //Hooks
+    View first,second,third,fourth,fifth,sixth,seventh;
+    TextView slogan;
+    ImageView image;
+    //Animations
+    Animation topAnimantion,bottomAnimation,middleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        btn1=(Button) findViewById(R.id.btn);
+        //Hooks
+        first = findViewById(R.id.first_line);
+        second = findViewById(R.id.second_line);
+        third = findViewById(R.id.third_line);
+        fourth = findViewById(R.id.fourth_line);
+        fifth = findViewById(R.id.fifth_line);
+        sixth = findViewById(R.id.sixth_line);
+        seventh= findViewById(R.id.seventh_line);
+        image = findViewById(R.id.img);
+        slogan = findViewById(R.id.tagLine);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        //Animation Calls
+        topAnimantion = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_animation);
+
+        //-----------Setting Animations to the elements of Splash Screen--------
+        first.setAnimation(topAnimantion);
+        second.setAnimation(topAnimantion);
+        third.setAnimation(topAnimantion);
+        fourth.setAnimation(topAnimantion);
+        fifth.setAnimation(topAnimantion);
+        sixth.setAnimation(topAnimantion);
+        seventh.setAnimation(topAnimantion);
+
+        image.setAnimation(middleAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        //Splash Screen Code to call new Activity after some time
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Category.class);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Category.class);
                 startActivity(intent);
+                finish();
             }
-        });
+        }, SPLASH_TIME_OUT);
+
     }
 }
